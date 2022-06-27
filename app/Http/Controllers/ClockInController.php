baae41fdf8883a576d760c2888ed4b2bd0319974
liuderu <wintergoes@163.com>
2022-06-27 10:26:36 +0800

    考勤统计修改

--- app/Http/Controllers/ClockInController.php
+++ app/Http/Controllers/ClockInController.php
@@ -6,9 +6,62 @@ use Illuminate\Http\Request;
 
 use App\ClockIn;
 
+use App\User;
+
+use DB;
+
 class ClockInController extends Controller
 {
     function index(Request $request){
+        $searchfromdate = "";
+        if($request->has("fromdate")){
+            $searchfromdate = $request->fromdate;
+        }
+
+        $searchtodate = "";
+        if($request->has("todate")){
+            $searchtodate = $request->todate ;
+        } 
         
+        $searchuserid = "";
+        if($request->has("userid")){
+            $searchuserid = $request->userid;
+        }
+        
+        $sqlstr = "select c.clockindate,c.userid,u.realname, sb.sbtime,xb.xbtime from (select distinct(date(created_at)) as clockindate,userid from clockins) as c "
+                . " left join (select min(created_at) sbtime,  userid as sbuserid from clockins where citype=1 group by date(created_at),userid) as sb "
+                . " on date(sb.sbtime)=c.clockindate and sb.sbuserid=c.userid "
+                . " left join (select max(created_at) as xbtime,userid as xbuserid from clockins where citype=2 group by date(created_at), userid ) as xb "
+                . " on date(xb.xbtime)=c.clockindate and xb.xbuserid=c.userid "
+                . " left join users as u on u.id=c.userid where 1=1 ";
+        
+        if($searchfromdate != ""){
+            $sqlstr .= " and c.clockindate >='" . $searchfromdate . "'";
+        }
+        
+        if($searchtodate != ""){
+            $sqlstr .= " and c.clockindate <='" . $searchtodate . "'";
+        }
+        
+        if($searchuserid != "" && $searchuserid != "-1"){
+            $sqlstr .= " and c.userid=" . $searchuserid;
+        }
+        
+        $sqlstr .=  " order by c.clockindate desc ";
+        
+        $clockins = DB::select($sqlstr);
+        
+        $users = User::orderBy("id", "asc")
+                ->where("username", "<>", "admin")
+                ->select("id", "realname")
+                ->get();
+        
+        return view("/stat/clockins", [
+            "clockins"=>$clockins,
+            "users"=>$users,
+            "searchfromdate"=>$searchfromdate,
+            "searchtodate"=>$searchtodate,
+            "searchuserid"=>$searchuserid
+        ]);
     }
 }
--- resources/views/stat/clockins.blade.php
+++ resources/views/stat/clockins.blade.php
@@ -4,29 +4,25 @@
 <script language="javascript" type="text/javascript" src="/js/dateutils.js"></script>
 <script language="javascript" type="text/javascript" src="/js/My97DatePicker/WdatePicker.js"></script>
 
-@if($logtype == 0)
-<h5 class="card-title">系统日志</h5>
-@else
-<h5 class="card-title">BSM日志</h5>
-@endif
+<h5 class="card-title">考勤统计</h5>
 <hr>
 
 <div class="row mb-4">
         <form id="form1" class="form-horizontal" method="get" >
             <input type="hidden" name="_token" value="ZaVg5KCerjuZCgDytKghF6Jc2kLHYSyMRo4MCXTB">
-            <input type="hidden" name="logtype" value="{{$logtype}}" />
             <table style="font-size: 12px; text-align: center;" >
                 <tr>                   
                     <td class="search_td">日期 自&nbsp;&nbsp;</td>
-                    <td class="search_td"><input name="fromdate" class="form-control" onClick="WdatePicker({el:this,dateFmt:'yyyy-MM-dd HH:mm:ss'})" autocomplete="off" size="16" value="{{$searchfromdate}}"/></td>
+                    <td class="search_td"><input name="fromdate" class="form-control" onClick="WdatePicker({el:this,dateFmt:'yyyy-MM-dd'})" autocomplete="off" size="16" value="{{$searchfromdate}}"/></td>
                     <td class="search_td">&nbsp;&nbsp;至&nbsp;&nbsp;</td>
-                    <td class="search_td"><input name="todate" class="form-control" onClick="WdatePicker({el:this,dateFmt:'yyyy-MM-dd HH:mm:ss'})" autocomplete="off" size="16" value="{{$searchtodate}}"/></td>
+                    <td class="search_td"><input name="todate" class="form-control" onClick="WdatePicker({el:this,dateFmt:'yyyy-MM-dd'})" autocomplete="off" size="16" value="{{$searchtodate}}"/></td>
                   
-                    <td class="search_td">&nbsp;&nbsp;设备编号&nbsp;&nbsp;</td>
+                    <td class="search_td">&nbsp;&nbsp;人员&nbsp;&nbsp;</td>
                     <td class="search_td">
-                        <select name="devicecode" class="form-select"  style="width: 200px">
-                            @foreach($devices as $device)
-                            <option class="form-control" value="{{$device->id}}" {{$searchdevicecode == $device->id ? "selected" : ""}}>{{$device->devicecode}}</option>
+                        <select name="userid" class="form-select" >
+                            <option class="form-control" value="-1" {{$searchuserid == -1 ? "selected" : ""}}>不限</option>
+                            @foreach($users as $user)
+                            <option class="form-control" value="{{$user->id}}" {{$searchuserid == $user->id ? "selected" : ""}}>{{$user->realname}}</option>
                             @endforeach
                         </select>
                     </td>                    
@@ -36,26 +32,27 @@
         </form>
 </div>
 
-<style>
-a:visited{color: #F03C96;}    
-</style>
-
 <div  class="dataTables_wrapper dt-bootstrap5">
     <div class="row">
         <div class="col-sm-auto">
-        <table class="table mb-0 table-hover table-bordered" style="width: 100%">
-                <tbody><?php $lfcount = 0 ?>
+        <table class="table mb-0 table-hover table-bordered" >
+                <thead>
+                    <tr role="row">
+                        <th >姓名</th>
+                        <th >日期</th>
+                        <th >上班时间</th>
+                        <th >下班时间</th>
+                    </tr>
+                </thead>            
+                <tbody>
+                    @foreach($clockins as $ci)
                     <tr>
-                        @foreach($logfiles as $logfile)
-                        <td style="font-size: 12px">
-                            <nobr><a href="{{$logtype == 0 ? "device" : "bsm"}}logfilecontent?filename={{$logfile->logfile}}&devicecode={{$searchdevicename}}&deviceid={{$searchdevicecode}}&logtype={{$logtype}}" target="_blank">{{$logfile->logfile}}</a> 
-                            <a href="dllogfile?filename={{$logfile->logfile}}&devicecode={{$searchdevicename}}&deviceid={{$searchdevicecode}}&logtype={{$logtype}}" target="_blank"><img src="images/dllogfile.png" width="20px" height="20px"></a></nobr>
-                        </td>
-
-                        <?php $lfcount++ ?>
-                        {!!$lfcount % 5 == 0 ? "</tr><tr>" : ""!!}
-                        @endforeach
-                    </tr>           
+                        <td>{{$ci->realname}}</td>
+                        <td>{{$ci->clockindate}}</td>
+                        <td>{{$ci->sbtime}}</td>
+                        <td>{{$ci->xbtime}}</td>
+                    </tr>
+                    @endforeach
                 </tbody>
         </table>    
         </div>
